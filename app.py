f1a4a8f0ea3779bffc8ca259784b21ef52f22d97
Sazz-75 <samuelvictoralves0@gmail.com>
2025-09-09 14:16:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st 
+
+
+st.title("meus projetos 😍")
+
+st.markdown(
+"""  
+###
+# 
+*KONISEGG JESKO* 
+     
+    
+    
+"""  
+    ) 
+
+st.caption("criado com streamlit ❤️")
+
+st.image("https://www.hdcarwallpapers.com/download/koenigsegg_jesko_5k-3840x2160.jpg")
+
+st.video("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj9q6jUybqPAxVPB7kGHU9TPcEQtwJ6BAgQEAI&url=https%3A%2F%2Fwww.youtube.com%2Fshorts%2Fawwpakngf38&usg=AOvVaw2SEktFhNqRYPKzi_b2BC3L&opi=89978449")
+
+st.markdown(
+"""  
+###
+# 
+*KONISEGG JESKO* 
+     
+    
+    
+"""  
+    ) 
